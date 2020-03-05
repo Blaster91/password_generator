01b18e001b4347102d403c0be4b9b6647f41dae7
Blaster91 <r.leroy@codeur.online>
2020-03-05 02:21:53 +0100

    add files

--- password_generator.py
+++ password_generator.py
@@ -2,24 +2,36 @@ from tkinter import *
 
 window = Tk()
 
-window.title("My Application")
+window.title("Générateur de mot de passe")
 window.geometry("720x480")
 window.minsize(480, 360)
-window.iconbitmap("scum.ico")
+window.iconbitmap("password.ico")
 window.config(background="#4065A4")
 
+#creer la frame principale
 frame = Frame(window, bg='#4065A4')
 
 # creation d'image
 width = 300
 height = 300
 image = PhotoImage(file="password_onbording.png").zoom(35).subsample(80)
-canvas = Canvas(window, width=width, height=height, bg='#4065A4')
+canvas = Canvas(frame, width=width, height=height, bg='#4065A4', bd=0, highlightthickness=0)
 canvas.create_image(width/2, height/2, image=image)
-canvas.pack(expand=YES)
+canvas.grid(row=0, column=0, sticky=W)
 
-# creer un titre
-label_title = Label(frame, text="Mot de pass", font=("Helvetica", 20), bg="#4065A4", fg='white
+#creer une sous boite
+right_frame = Frame(frame, bg='#4065A4')
+
+#creer un titre
+label_title = Label(right_frame, text="Mot de passe", font=("Helvetica", 20), bg="#4065A4", fg='white')
+label_title.grid(row=0,column=1,sticky=W)
+
+# creer un champs/entr
+password_entry = Entry(right_frame, font=("Helvetica", 20), bg="#4065A4", fg='white')
+password_entry.pack()
+
+# on place la sous boite à droite de la frame principal
+right_frame.grid(row=0, column=1, sticky=W)
 
 # afficher la frame
 frame.pack(expand=YES)

9676fb6af5a8f84a04ae3024d6ad06303bdc1c3d
Blaster91 <r.leroy@codeur.online>
2020-03-08 15:31:58 +0100

    add function generate password

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,19 @@
+import string
+from random import randint, choice
 from tkinter import *
 
-window = Tk()
+def generate_password():
+    password_min = 6
+    password_max = 12
+    all_chars = string.ascii_letters + string.punctuation + string.digits
 
+    password = "".join(choice(all_chars) for x in range(randint(password_min, password_max)))
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
+
+
+# creer la fenetre
+window = Tk()
 window.title("Générateur de mot de passe")
 window.geometry("720x480")
 window.minsize(480, 360)
@@ -24,11 +36,15 @@ right_frame = Frame(frame, bg='#4065A4')
 
 #creer un titre
 label_title = Label(right_frame, text="Mot de passe", font=("Helvetica", 20), bg="#4065A4", fg='white')
-label_title.grid(row=0,column=1,sticky=W)
+label_title.pack()
 
-# creer un champs/entr
+# creer un champs/entrée/inp
 password_entry = Entry(right_frame, font=("Helvetica", 20), bg="#4065A4", fg='white')
 password_entry.pack()
+
+#creer un bouton
+generate_password_button = Button(right_frame, text="Générer", font=("Helvetica", 20), bg="#4065A4", fg='white', command=generate_password)
+generate_password_button.pack(fill=X)
 
 # on place la sous boite à droite de la frame principal
 right_frame.grid(row=0, column=1, sticky=W)
